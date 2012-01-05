67cf43ea1a5e43d0750e5f7fa5fc470b54007fa4
Robert Nevels <geomasta21@aol.com>
2012-01-05 02:03:00 -0600

    added jquery effect, centered page, tried to resize hieght

--- school_main.js
+++ school_main.js
@@ -1,8 +1,16 @@
 function startup() {
+	window.onresize = handleResize;
 	setInitContent();
 	manageCounter();
 }
 
+function handleResize() {
+	var size = getSize();
+	console.log(size.height+","+size.width);
+	console.log($("#mainContent"));
+}
+
+// everyone considered him the coward of the county
 function manageCounter() {
 	var callback = function(contents) {
 		var counter = parseInt(contents)+1;
@@ -14,12 +22,13 @@ function manageCounter() {
 
 function setInitContent() {
 	var stored = getCookie("robz-utcs-webpage-index");
-	if (stored==null) 
+	if (!stored) 
 		setContent(0);
 	else
 		setContent(stored);
 }
 
+var starting = true;
 function setContent(index) {
 	var title = "error! ",
 		text = "this shouldn't be happening: "+index;
@@ -28,6 +37,20 @@ function setContent(index) {
 		text = content_arr[index]["text"];
 		setCookie("robz-utcs-webpage-index",index);
 	}
+	
+	if (!starting) {
+		$("#mainContent").fadeOut(150);
+		setTimeout(function(){
+			setContentAux(title,text);
+			$("#mainContent").fadeIn(150);
+		}, 150);
+	} else {
+		starting = false;
+		setContentAux(title,text);
+	} 
+}
+
+function setContentAux(title, text) {
 	document.getElementById("contentHeading").innerHTML = title;
 	document.getElementById("contentText").innerHTML = text;
 }
