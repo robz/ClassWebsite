93c8155ec66f77f12d53d93f55d2abba452f526c
Robby <geomasta21@aol.com>
2012-01-08 17:55:11 -0600

    fixed weird path issues

--- external/write.php
+++ external/write.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_POST['text'];
-	$FILENAME = $_POST['filename'];
+	$FILENAME = '../'.$_POST['filename'];
 	$HANDLE = fopen($FILENAME, 'w') or die ('CANT OPEN FILE');
 	fwrite($HANDLE, $_POST['text']);
 	fclose($HANDLE);
